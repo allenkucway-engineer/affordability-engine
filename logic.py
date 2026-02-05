8f1ec109dee3fff15a8ec810a14f47a05da9af01
allenkucway-engineer <allenkucway@gmail.com>
2026-02-04 20:53:55 -0500

    Modify affordable_payment to include ratio parameter
    Updated affordable_payment function to accept a ratio parameter for flexibility.

--- logic.py
+++ logic.py
@@ -1,2 +1,5 @@
-def affordable_payment(income):
+def affordable_payment(income, ratio=0.30):
+  """
+  Return a recommended affordable payment as a percentage of income. Default ratio is 30% (0.30).
+  """
   return income * 0.30
